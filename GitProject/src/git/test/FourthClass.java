d7af1c36c1116fd5e560e42657a816f50a0599d3
Ciro Rivieccio <ciro.rivieccio@gmail.com>
2011-08-30 10:32:50 +0100

    Added new class to project

---
+++ GitProject/src/git/test/FourthClass.java
@@ -0,0 +1,14 @@
+package git.test;
+
+public class FourthClass {
+	private String strVar1;
+
+	public String getStrVar1() {
+		return strVar1;
+	}
+
+	public void setStrVar1(String strVar1) {
+		this.strVar1 = strVar1;
+	}
+
+}
