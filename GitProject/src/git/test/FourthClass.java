ecbbbff8e697f4b112dae4c24fa65387e86bef02
Ciro Rivieccio <ciro.rivieccio@gmail.com>
2011-08-30 10:39:18 +0100

    Added float variable to that class

--- GitProject/src/git/test/FourthClass.java
+++ GitProject/src/git/test/FourthClass.java
@@ -3,6 +3,7 @@ package git.test;
 public class FourthClass {
 	private String strVar1;
 	private int intVar1;
+	private float floatVar1;
 
 	public String getStrVar1() {
 		return strVar1;
@@ -19,5 +20,15 @@ public class FourthClass {
 	public void setIntVar1(int intVar1) {
 		this.intVar1 = intVar1;
 	}
+
+	public float getFloatVar1() {
+		return floatVar1;
+	}
+
+	public void setFloatVar1(float floatVar1) {
+		this.floatVar1 = floatVar1;
+	}
+	
+	
 
 }
