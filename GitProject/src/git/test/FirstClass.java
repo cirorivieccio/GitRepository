4f5fdf037cb7fd34282a6d75013dad76148ab5d2
Ciro Rivieccio <ciro.rivieccio@gmail.com>
2011-08-30 11:19:44 +0100

    Modified classes FirstClass and SecondClass in master branch

--- GitProject/src/git/test/FirstClass.java
+++ GitProject/src/git/test/FirstClass.java
@@ -1,5 +1,41 @@
 package git.test;
 
 public class FirstClass {
+	private int intVar1;
+	private int intVar2;
+	private String stringVar1;
+	private String stringVar2;
+
+	public int getIntVar1() {
+		return intVar1;
+	}
+
+	public void setIntVar1(int intVar1) {
+		this.intVar1 = intVar1;
+	}
+
+	public int getIntVar2() {
+		return intVar2;
+	}
+
+	public void setIntVar2(int intVar2) {
+		this.intVar2 = intVar2;
+	}
+
+	public String getStringVar1() {
+		return stringVar1;
+	}
+
+	public void setStringVar1(String stringVar1) {
+		this.stringVar1 = stringVar1;
+	}
+
+	public String getStringVar2() {
+		return stringVar2;
+	}
+
+	public void setStringVar2(String stringVar2) {
+		this.stringVar2 = stringVar2;
+	}
 
 }
--- GitProject/src/git/test/SecondClass.java
+++ GitProject/src/git/test/SecondClass.java
@@ -1,5 +1,23 @@
 package git.test;
 
 public class SecondClass {
+	private int intVar2;
+	private int intVar3;
+
+	public int getIntVar2() {
+		return intVar2;
+	}
+
+	public void setIntVar2(int intVar2) {
+		this.intVar2 = intVar2;
+	}
+
+	public int getIntVar3() {
+		return intVar3;
+	}
+
+	public void setIntVar3(int intVar3) {
+		this.intVar3 = intVar3;
+	}
 
 }
