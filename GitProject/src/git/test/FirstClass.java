ef317f76e7a3c36b6532579ccc4d5422c16dc952
Ciro Rivieccio <ciro.rivieccio@gmail.com>
2011-08-30 11:09:07 +0100

    Modified class FirstClass and SecondClass in branch1.

--- GitProject/src/git/test/FirstClass.java
+++ GitProject/src/git/test/FirstClass.java
@@ -1,5 +1,22 @@
 package git.test;
 
 public class FirstClass {
+	private int intVar1;
+	private int intVar2;
 
+	public int getIntVar1() {
+		return intVar1;
+	}
+
+	public void setIntVar1(int intVar1) {
+		this.intVar1 = intVar1;
+	}
+
+	public int getIntVar2() {
+		return intVar2;
+	}
+
+	public void setIntVar2(int intVar2) {
+		this.intVar2 = intVar2;
+	}
 }
