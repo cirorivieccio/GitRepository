0962d6683f5825129f73b5c800817ef47683ab8e
Ciro Rivieccio <ciro.rivieccio@gmail.com>
2011-08-30 10:35:44 +0100

    Added int variable

--- GitProject/src/git/test/FourthClass.java
+++ GitProject/src/git/test/FourthClass.java
@@ -2,6 +2,7 @@ package git.test;
 
 public class FourthClass {
 	private String strVar1;
+	private int intVar1;
 
 	public String getStrVar1() {
 		return strVar1;
@@ -9,6 +10,14 @@ public class FourthClass {
 
 	public void setStrVar1(String strVar1) {
 		this.strVar1 = strVar1;
+	}
+
+	public int getIntVar1() {
+		return intVar1;
+	}
+
+	public void setIntVar1(int intVar1) {
+		this.intVar1 = intVar1;
 	}
 
 }
